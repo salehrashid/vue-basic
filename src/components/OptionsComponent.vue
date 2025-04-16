a02644a46546d24cc1f359467351fa654521a852
salehrashid <saleh.rashid.b@gmail.com>
2025-04-16 17:50:47 +0700

    feat(components): add options and compositions api

---
+++ src/components/OptionsComponent.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    data(){
+        return {
+            name: "Saleh",
+            umur: 20,
+            alamat: "Bogor"
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>Options API</h1>
+    <h1>halo, perkenalkan nama saya {{ name }}, umur saya {{ umur }}, saya tinggal di {{ alamat }}</h1>
+</template>
